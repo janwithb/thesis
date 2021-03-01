0210c971f8bfcefa322589d8bf4498cf9872b71b
Jan Bieser <12jan37@gmail.com>
2021-03-01 17:33:44 +0100

    changed observation dimensions

--- utils/frame_stack_wrapper.py
+++ utils/frame_stack_wrapper.py
@@ -10,13 +10,13 @@ class FrameStack(Wrapper):
         super(FrameStack, self).__init__(env)
         self._num_stack = num_stack
         self._frames = deque(maxlen=num_stack)
-        low = np.repeat(self.observation_space.low[np.newaxis, ...], self._num_stack, axis=0)
-        high = np.repeat(self.observation_space.high[np.newaxis, ...], self._num_stack, axis=0)
+        low = np.repeat(self.observation_space.low, self._num_stack, axis=0)
+        high = np.repeat(self.observation_space.high, self._num_stack, axis=0)
         self.observation_space = Box(low=low, high=high, dtype=self.observation_space.dtype)
 
     def _get_observation(self):
         assert len(self._frames) == self._num_stack, (len(self._frames), self._num_stack)
-        return np.array(self._frames)
+        return np.array(np.concatenate(self._frames))
 
     def step(self, action):
         observation, reward, done, info = self.env.step(action)
--- utils/pixel_observation_wrapper.py
+++ utils/pixel_observation_wrapper.py
@@ -6,41 +6,50 @@ from gym import ObservationWrapper
 
 
 class PixelObservationWrapper(ObservationWrapper):
-    def __init__(self, env, preprocessing=True, observation_size=100):
+    def __init__(self, env, crop_center_observation=True, resize_observation=True, observation_size=100,
+                 grayscale_observation=True, normalize_observation=True):
         super(PixelObservationWrapper, self).__init__(env)
+
+        self._env = env
+        self._crop_center_observation = crop_center_observation
+        self._resize_observation = resize_observation
+        self._observation_size = observation_size
+        self._grayscale_observation = grayscale_observation
+        self._normalize_observation = normalize_observation
 
         # extend observation space with pixels
         env.reset()
-        pixels = self.env.render(mode='rgb_array')
+        pixels = self.preprocess_observation(self.env.render(mode='rgb_array'))
         if np.issubdtype(pixels.dtype, np.integer):
             low, high = (0, 255)
-        elif np.issubdtype(pixels.dtype, np.float):
-            low, high = (-float('inf'), float('inf'))
+        elif np.issubdtype(pixels.dtype, np.float32):
+            low, high = (np.float32(0), np.float32(1))
         else:
             raise TypeError(pixels.dtype)
         pixels_space = spaces.Box(shape=pixels.shape, low=low, high=high, dtype=pixels.dtype)
         self.observation_space = pixels_space
 
-        self._env = env
-        self._preprocessing = preprocessing
-        self._observation_size = observation_size
-
     def observation(self, observation):
         pixel_observation = self._env.render(mode='rgb_array')
-        if self._preprocessing:
-            prepocessed_observation = self._preprocess_observation(pixel_observation)
-            return prepocessed_observation
-        else:
-            return pixel_observation
-
-    def _preprocess_observation(self, observation):
-        prepocessed_observation = self._crop_center_observation(observation)
-        prepocessed_observation = self._resize_observation(prepocessed_observation)
-        prepocessed_observation = self._grayscale_observation(prepocessed_observation)
-        prepocessed_observation = self._normalize_observation(prepocessed_observation)
+        prepocessed_observation = self.preprocess_observation(pixel_observation)
         return prepocessed_observation
 
-    def _crop_center_observation(self, observation):
+    def preprocess_observation(self, observation):
+        prepocessed_observation = observation
+        if self._crop_center_observation:
+            prepocessed_observation = self.crop_center_observation(observation)
+        if self._resize_observation:
+            prepocessed_observation = self.resize_observation(prepocessed_observation)
+        if self._grayscale_observation:
+            prepocessed_observation = self.grayscale_observation(prepocessed_observation)
+        if self._normalize_observation:
+            prepocessed_observation = self.normalize_observation(prepocessed_observation)
+
+        # change shape from (height, width, channels) to (channels, height, width)
+        prepocessed_observation = np.rollaxis(prepocessed_observation, 2, 0)
+        return prepocessed_observation
+
+    def crop_center_observation(self, observation):
         pil_img = Image.fromarray(observation)
         crop_width, crop_height = min(pil_img.size), min(pil_img.size)
         img_width, img_height = pil_img.size
@@ -50,15 +59,15 @@ class PixelObservationWrapper(ObservationWrapper):
                                         (img_height + crop_height) // 2))
         return np.asarray(cropped_pil_img)
 
-    def _resize_observation(self, observation):
+    def resize_observation(self, observation):
         pil_img = Image.fromarray(observation)
         resized_pil_img = pil_img.resize((self._observation_size, self._observation_size))
         return np.asarray(resized_pil_img)
 
-    def _grayscale_observation(self, observation):
+    def grayscale_observation(self, observation):
         pil_img = Image.fromarray(observation)
-        grayscale_pil_img = pil_img.convert('L')
-        return np.asarray(grayscale_pil_img)
+        grayscale_pil_img = np.expand_dims(np.asarray(pil_img.convert('L')), 2)
+        return grayscale_pil_img
 
-    def _normalize_observation(self, observation):
+    def normalize_observation(self, observation):
         return np.array(observation).astype(np.float32) / 255.0
