a76b79b10cd00a349aa36d74873b8b3a0f53a36b
Jan Bieser <12jan37@gmail.com>
2021-03-03 18:17:21 +0100

    misc functions

---
+++ utils/misc.py
@@ -0,0 +1,169 @@
+import json
+import os
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as fu
+
+from typing import Iterable
+from torch.nn import Module
+
+from models.rssm import RSSMState
+
+
+def make_dir(dir_path):
+    try:
+        os.mkdir(dir_path)
+    except OSError:
+        pass
+    return dir_path
+
+
+def save_config(config_dir, args):
+    with open(os.path.join(config_dir, 'args.json'), 'w') as f:
+        json.dump(vars(args), f, sort_keys=True, indent=4)
+
+
+def weight_init(m):
+    """Custom weight init for Conv2D and Linear layers."""
+    if isinstance(m, nn.Linear):
+        nn.init.orthogonal_(m.weight.data)
+        m.bias.data.fill_(0.0)
+    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        assert m.weight.size(2) == m.weight.size(3)
+        m.weight.data.fill_(0.0)
+        m.bias.data.fill_(0.0)
+        mid = m.weight.size(2) // 2
+        gain = nn.init.calculate_gain('relu')
+        nn.init.orthogonal_(m.weight.data[:, :, mid, mid], gain)
+
+
+def gaussian_logprob(noise, log_std):
+    """Compute Gaussian log probability."""
+    residual = (-0.5 * noise.pow(2) - log_std).sum(-1, keepdim=True)
+    return residual - 0.5 * np.log(2 * np.pi) * noise.size(-1)
+
+
+def squash(mu, pi, log_pi):
+    """Apply squashing function.
+    See appendix C from https://arxiv.org/pdf/1812.05905.pdf.
+    """
+    mu = torch.tanh(mu)
+    if pi is not None:
+        pi = torch.tanh(pi)
+    if log_pi is not None:
+        log_pi -= torch.log(fu.relu(1 - pi.pow(2)) + 1e-6).sum(-1, keepdim=True)
+    return mu, pi, log_pi
+
+
+def tie_weights(src, trg):
+    assert type(src) == type(trg)
+    trg.weight = src.weight
+    trg.bias = src.bias
+
+
+def get_parameters(modules: Iterable[Module]):
+    """
+    Given a list of torch modules, returns a list of their parameters.
+    :param modules: iterable of modules
+    :returns: a list of parameters
+    """
+    model_parameters = []
+    for module in modules:
+        model_parameters += list(module.parameters())
+    return model_parameters
+
+
+def conv_out(h_in, padding, kernel_size, stride):
+    return int((h_in + 2. * padding - (kernel_size - 1.) - 1.) / stride + 1.)
+
+
+def output_padding(h_in, conv_out, padding, kernel_size, stride):
+    return h_in - (conv_out - 1) * stride + 2 * padding - (kernel_size - 1) - 1
+
+
+def conv_out_shape(h_in, padding, kernel_size, stride):
+    return tuple(conv_out(x, padding, kernel_size, stride) for x in h_in)
+
+
+def output_padding_shape(h_in, conv_out, padding, kernel_size, stride):
+    return tuple(output_padding(h_in[i], conv_out[i], padding, kernel_size, stride) for i in range(len(h_in)))
+
+
+def infer_leading_dims(tensor, dim):
+    """Looks for up to two leading dimensions in ``tensor``, before
+    the data dimensions, of which there are assumed to be ``dim`` number.
+    For use at beginning of model's ``forward()`` method, which should
+    finish with ``restore_leading_dims()`` (see that function for help.)
+    Returns:
+    lead_dim: int --number of leading dims found.
+    T: int --size of first leading dim, if two leading dims, o/w 1.
+    B: int --size of first leading dim if one, second leading dim if two, o/w 1.
+    shape: tensor shape after leading dims.
+    """
+    lead_dim = tensor.dim() - dim
+    assert lead_dim in (0, 1, 2)
+    if lead_dim == 2:
+        T, B = tensor.shape[:2]
+    else:
+        T = 1
+        B = 1 if lead_dim == 0 else tensor.shape[0]
+    shape = tensor.shape[lead_dim:]
+    return lead_dim, T, B, shape
+
+
+class FreezeParameters:
+    def __init__(self, modules: Iterable[Module]):
+        """
+        Context manager to locally freeze gradients.
+        In some cases with can speed up computation because gradients aren't calculated for these listed modules.
+        example:
+        ```
+        with FreezeParameters([module]):
+            output_tensor = module(input_tensor)
+        ```
+        :param modules: iterable of modules. used to call .parameters() to freeze gradients.
+        """
+        self.modules = modules
+        self.param_states = [p.requires_grad for p in get_parameters(self.modules)]
+
+    def __enter__(self):
+        for param in get_parameters(self.modules):
+            param.requires_grad = False
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for i, param in enumerate(get_parameters(self.modules)):
+            param.requires_grad = self.param_states[i]
+
+
+def compute_return(reward: torch.Tensor,
+                   value: torch.Tensor,
+                   discount: torch.Tensor,
+                   bootstrap: torch.Tensor,
+                   lambda_: float):
+    """
+    Compute the discounted reward for a batch of data.
+    reward, value, and discount are all shape [horizon - 1, batch, 1] (last element is cut off)
+    Bootstrap is [batch, 1]
+    """
+    next_values = torch.cat([value[1:], bootstrap[None]], 0)
+    target = reward + discount * next_values * (1 - lambda_)
+    timesteps = list(range(reward.shape[0] - 1, -1, -1))
+    outputs = []
+    accumulated_reward = bootstrap
+    for t in timesteps:
+        inp = target[t]
+        discount_factor = discount[t]
+        accumulated_reward = inp + discount_factor * lambda_ * accumulated_reward
+        outputs.append(accumulated_reward)
+    returns = torch.flip(torch.stack(outputs), [0])
+    return returns
+
+
+def flatten_rssm_state(state: RSSMState):
+    state.mean = torch.flatten(state.mean, start_dim=0, end_dim=1)
+    state.std = torch.flatten(state.std, start_dim=0, end_dim=1)
+    state.stoch = torch.flatten(state.stoch, start_dim=0, end_dim=1)
+    state.deter = torch.flatten(state.deter, start_dim=0, end_dim=1)
+    return state
