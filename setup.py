8a1e293747e5cf5b0ddabd0398db252c80c6d13c
Jan Bieser <12jan37@gmail.com>
2021-03-08 20:02:23 +0100

    setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='thesis',
+    version='',
+    packages=['algos', 'models', 'scripts'],
+    url='',
+    license='',
+    author='jan',
+    author_email='',
+    description=''
+)
