ea0aa0194bb79f013a51cd2a7c928d810c47b13c
Jan Bieser <12jan37@gmail.com>
2021-04-28 12:38:20 +0200

    added envs package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='thesis',
     version='',
-    packages=['algos', 'models', 'scripts', 'agents', 'utils', 'wrappers'],
+    packages=['algos', 'models', 'scripts', 'agents', 'utils', 'wrappers', 'envs'],
     url='',
     license='',
     author='jan',
